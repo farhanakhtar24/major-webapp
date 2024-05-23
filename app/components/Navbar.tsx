61039041211c5241017fcf5d30eb787a8079a10f
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2024-05-23 17:30:39 +0530

    updates interface

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,60 +1,58 @@
-'use client'
-import React from 'react'
-import {RxHamburgerMenu} from 'react-icons/rx'
-import {AiOutlineClose} from 'react-icons/ai'
+"use client";
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
+	const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+	return (
+		<nav className="w-screen bg-[#8eb8d0] h-fit overflow-hidden">
+			<div className="py-4 lg:px-8 px-4 max-w-[1280px] h-[10vh] m-auto text-white flex items-center justify-between">
+				<div>
+					<h1 className="lg:text-2xl text-xl uppercase tracking-wider cursor-pointer font-bold">
+						Image Super-resolution
+					</h1>
+				</div>
+				<div className="md:flex hidden lg:gap-8 gap-6 uppercase tracking-wider cursor-pointer text-lg items-center">
+					<span className="group">
+						Services
+						<div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
+					</span>
+					<span className="group">
+						About
+						<div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
+					</span>
+					<span className="group">
+						Contact
+						<div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
+					</span>
+				</div>
+				<div className="md:hidden flex items-center text-xl">
+					{isMenuOpen ? (
+						<AiOutlineClose onClick={toggleMenu} />
+					) : (
+						<RxHamburgerMenu onClick={toggleMenu} />
+					)}
+				</div>
+				<div
+					id="mobileNav"
+					className={`fixed flex flex-col gap-8 pt-16 px-4 text-xl uppercase bg-teal-500 h-full inset-0 top-16 w-[70%] ${
+						isMenuOpen ? "left-0" : "left-[-70%]"
+					} ease-in-out duration-500 cursor-pointer`}>
+					<span>Services</span>
+					<span>About</span>
+					<span>Contact</span>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
-  return (
-    <nav className="w-screen bg-[#8eb8d0] h-fit overflow-hidden">
-      <div className="py-4 lg:px-8 px-4 max-w-[1280px] h-[10vh] m-auto text-white flex items-center justify-between">
-        <div>
-          <h1 className="lg:text-2xl text-xl uppercase tracking-wider cursor-pointer font-bold">
-            Image De-Rain
-          </h1>
-        </div>
-        <div className="md:flex hidden lg:gap-8 gap-6 uppercase tracking-wider cursor-pointer text-lg items-center">
-          <span className="group">
-            Services
-            <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
-          </span>
-          <span className="group">
-            About
-            <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
-          </span>
-          <span className="group">
-            Contact
-            <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
-          </span>
-        </div>
-        <div className="md:hidden flex items-center text-xl">
-          {isMenuOpen ? (
-            <AiOutlineClose onClick={toggleMenu} />
-          ) : (
-            <RxHamburgerMenu onClick={toggleMenu} />
-          )}
-        </div>
-        <div
-          id="mobileNav"
-          className={`fixed flex flex-col gap-8 pt-16 px-4 text-xl uppercase bg-teal-500 h-full inset-0 top-16 w-[70%] ${
-            isMenuOpen ? "left-0" : "left-[-70%]"
-          } ease-in-out duration-500 cursor-pointer`}
-        >
-          <span>Services</span>
-          <span>About</span>
-          <span>Contact</span>
-        </div>
-      </div>
-    </nav>
-  );
-}
-
-export default Navbar
+export default Navbar;
--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,38 +1,42 @@
-import React from 'react'
-import {AiOutlineDoubleRight} from 'react-icons/ai'
-import LinkButton from '../components/LinkButton';
+import React from "react";
+import { AiOutlineDoubleRight } from "react-icons/ai";
+import LinkButton from "../components/LinkButton";
+import Image from "next/image";
+import OriginalImage from "@/public/Originalmage.png";
+import UpscaledImage from "@/public/UpscaledImage.png";
 
-type Props = {}
+type Props = {};
 
 const HomePage = (props: Props) => {
-    
-  return (
-    <div className="w-full h-[90vh] flex items-center justify-center bg-gradient-to-r from-white to-[#bcd5e3]">
-      <div className="flex-1 flex flex-col items-start justify-center pl-40">
-        <h1 className="text-black font-bold text-8xl">
-          Remove rain
-          <br />
-          streaks.
-        </h1>
-        <p className="text-[#a6a6a6] text-lg mb-2">
-          AI trained to de-rain any image with rain streaks.
-        </p>
-        <LinkButton />
-      </div>
-      <div className="flex-1 group relative">
-        <img
-          className="rounded-lg absolute -top-40 z-0 w-[500px] group-hover:z-20"
-          src="rain.png"
-          alt="rain"
-        />
-        <img
-          className="rounded-lg absolute -top-20 left-20 w-[500px] z-10"
-          src="deRained.png"
-          alt="rain"
-        />
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="w-full h-[90vh] flex items-center justify-center bg-gradient-to-r gap-5 from-white to-[#bcd5e3]">
+			<div className="flex-1 flex flex-col items-start justify-center pl-40 gap-4">
+				<h1 className="text-black font-bold text-5xl">
+					Upscale images using Super-resolution.
+				</h1>
+				<p className="text-[#a6a6a6] text-lg mb-2">
+					AI trained to upscale any image with ESRGAN.
+				</p>
+				<LinkButton />
+			</div>
+			<div className="flex-1 group relative">
+				<Image
+					className="rounded-lg absolute -top-40 z-0 w-[350px] h-auto group-hover:z-20"
+					src={OriginalImage}
+					alt="rain"
+					width={500}
+					height={500}
+				/>
+				<Image
+					className="rounded-lg absolute -top-20 left-20 w-[350px] h-auto z-10"
+					src={UpscaledImage}
+					alt="rain"
+					width={500}
+					height={500}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default HomePage;
